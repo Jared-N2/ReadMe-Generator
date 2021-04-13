00a58c9ed574bd3a79672b2456fca04a9f744119
Jared Neisen <Jared.Neisen@gmail.com>
2021-04-13 17:55:02 -0600

    making generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,126 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+let badge = ''
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'Enter project title:',
+    name: 'title',
+},
+{
+    type: 'input',
+    message: 'Describe your project:',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'Make section for table of contents.',
+    name: 'table',
+},
+{
+    type: 'input',
+    message: 'What did you install?',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'enter usage data',
+    name: 'usage',
+},
+{
+    type: 'list',
+    message: 'What License did you use?',
+    name: 'license',
+    choices: ['MIT License', 'GNU General Public License v3.0', 'Apache License 2.0']
+},
+{
+    type: 'input',
+    message: 'Who were the contributors?',
+    name: 'Contribution',
+},
+{
+    type: 'input',
+    message: 'How many tests have you run?',
+    name: 'tests',
+},
+{
+    type: 'input',
+    message: 'Enter any questions',
+    name: 'questions'
+}];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((response) => {
+            switch (response.license) {
+                case 'MIT License':
+                    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+                    break;
+
+                case 'GNU General Public License v3.0':
+                    badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+                    break;
+
+                case 'Apache License 2.0':
+                    badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+                    break;
+
+                default:
+                    break;
+            }
+
+            let template = (response) => `# Title
+            ${response.title}
+            \n
+           
+           ## Description
+            ${response.description} 
+            \n
+           
+           ## Table of Contents
+            ${response.table} 
+            \n
+           
+           ## What I used to intall
+            ${response.installation} 
+            \n
+           
+           ## Usage Data
+           ${response.usage} 
+           \n
+           
+           ## What license did you use?
+           ${response.license} 
+           \n 
+           \n
+           ${badge}
+
+           ## Who are the Contributors?
+           ${response.contribution} 
+           \n
+           
+           ## How many test runs?
+           ${response.tests} 
+           \n
+           
+           ## Any questions?
+           ${response.questions}`
+
+            // let template = ` # this is the ${response.title} \n \n ${badge} `
+
+
+            fs.writeFile("ReadMe.md", template, (error) =>
+                error ? console.error(error) : console.log('success'))
+
+        })
+
+}
+
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  
 `;
 }
+
+inquirer.prompt([
+  {
+      type: 'input',
+      section:'Enter project title:',
+      name:'title',
+  },
+  {
+      type: 'input',
+      message: 'Describe your project:',
+      name: 'description',
+  },
+  {
+      type: 'input',
+      message: 'Make section for table of contents.',
+      name: 'table',
+  },
+  {
+      type: 'input',
+      message: 'What did you install?',
+      name: 'installation',
+  },
+  {
+      type: 'input',
+      message: 'enter usage data',
+      name: 'usage',
+  },
+  {
+      type: 'input',
+      message: 'What License did you use?',
+      name: 'Liscense',
+  },
+  {
+      type: 'input',
+      message: 'Who were the contributors?',
+      name: 'Contribution',
+  },
+  {
+      type: 'input',
+      message: 'How many tests have you run?',
+      name: 'tests',
+  },
+  {
+    type: 'input',
+    message: 'Enter any questions',
+    name: 'questions'
+  }
+  
+])
 
 module.exports = generateMarkdown;
