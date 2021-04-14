16d1a8574dfb9168d44d31d76d325befd40f7058
Jared Neisen <Jared.Neisen@gmail.com>
2021-04-13 18:13:37 -0600

    trying to print readme

---
+++ Develop/ReadMe.md
@@ -0,0 +1,37 @@
+
+            # Title
+            j
+            
+            ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+           
+           ## Description
+            j 
+            
+           
+           ## Table of Contents
+            j 
+            
+           
+           ## What I used to intall
+            j 
+            
+           
+           ## Usage Data
+           j 
+           
+           
+           ## What license did you use?
+           MIT License 
+           
+           
+
+           ## Who are the Contributors?
+           undefined 
+           
+           
+           ## How many test runs?
+           j 
+           
+           
+           ## Any questions?
+           j
--- Develop/index.js
+++ Develop/index.js
@@ -37,7 +37,7 @@ const questions = [{
 {
     type: 'input',
     message: 'Who were the contributors?',
-    name: 'Contribution',
+    name: 'contribution',
 },
 {
     type: 'input',
@@ -59,62 +59,98 @@ function init() {
         .then((response) => {
             switch (response.license) {
                 case 'MIT License':
-                    badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+                    badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
                     break;
 
                 case 'GNU General Public License v3.0':
-                    badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+                    badge = '![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)'
                     break;
 
                 case 'Apache License 2.0':
-                    badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+                    badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
                     break;
 
                 default:
                     break;
             }
 
-            let template = (response) => `# Title
-            ${response.title}
-            \n
-           
-           ## Description
-            ${response.description} 
-            \n
-           
-           ## Table of Contents
-            ${response.table} 
-            \n
-           
-           ## What I used to intall
-            ${response.installation} 
-            \n
-           
-           ## Usage Data
-           ${response.usage} 
-           \n
-           
-           ## What license did you use?
-           ${response.license} 
-           \n 
-           \n
-           ${badge}
+            //     const template = (response) => `# Title
+            //     ${response.title}
+            //     \n
 
-           ## Who are the Contributors?
-           ${response.contribution} 
-           \n
-           
-           ## How many test runs?
-           ${response.tests} 
-           \n
-           
-           ## Any questions?
-           ${response.questions}`
+            //    ## Description
+            //     ${response.description} 
+            //     \n
+
+            //    ## Table of Contents
+            //     ${response.table} 
+            //     \n
+
+            //    ## What I used to intall
+            //     ${response.installation} 
+            //     \n
+
+            //    ## Usage Data
+            //    ${response.usage} 
+            //    \n
+
+            //    ## What license did you use?
+            //    ${response.license} 
+            //    \n 
+            //    \n
+            //    ${badge}
+
+            //    ## Who are the Contributors?
+            //    ${response.contribution} 
+            //    \n
+
+            //    ## How many test runs?
+            //    ${response.tests} 
+            //    \n
+
+            //    ## Any questions?
+            //    ${response.questions}`
+
 
             // let template = ` # this is the ${response.title} \n \n ${badge} `
 
 
-            fs.writeFile("ReadMe.md", template, (error) =>
+            fs.writeFile("ReadMe.md", `# Title
+            ${response.title}
+            
+            ${badge}
+           
+           ## Description
+            ${response.description} 
+            
+           
+           ## Table of Contents
+            ${response.table} 
+            
+           
+           ## What I used to intall
+            ${response.installation} 
+            
+           
+           ## Usage Data
+           ${response.usage} 
+           
+           
+           ## What license did you use?
+           ${response.license} 
+           
+           
+
+           ## Who are the Contributors?
+           ${response.contribution} 
+           
+           
+           ## How many test runs?
+           ${response.tests} 
+           
+           
+           ## Any questions?
+           ${response.questions}`, (error) =>
                 error ? console.error(error) : console.log('success'))
 
         })
